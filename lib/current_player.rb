69ec899c60190b0e88454e6c19e9f9537d6a1622
Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-03-16 03:46:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,10 @@ def turn_count(board)
   turn_count_player_one = 0
   turn_count_player_two = 0
   board.each do |move|
-    if move === "O"
-      turn_count_player_two += 1
-    else
+    if move === "X"
       turn_count_player_one += 1
+    elsif move === "O"
+      turn_count_player_two += 1
     end
   end
   turn_count_player_one + turn_count_player_two
